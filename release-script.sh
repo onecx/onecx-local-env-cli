f6c8aec0f7dbde5283d68cc439fbd8b3d53290aa
Benjamin Pabst <166110368+BenjaminPabst@users.noreply.github.com>
2025-02-17 10:16:44 +0100

    ci: updated ci

---
+++ release-script.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+echo "$1"
+echo "$2"
+export VERSION=$1
+export CHANNEL=$2
+packageJsonData=$(cat package.json)
+topLevelPackageVersion=$(echo "$packageJsonData" | jq -r '.version')
+if [[ $topLevelPackageVersion != $1 ]]
+then
+    npx -p replace-json-property rjp ./package.json version $1
+fi
+
+folder_names=()
+directory="libs"
+while IFS= read -r folder; do
+    folder_names+=("$folder")
+done < <(find "$directory" -mindepth 1 -maxdepth 1 -type d | awk -F "/" '{print $NF}' | sort | uniq)
+
+for folder in "${folder_names[@]}"; do
+    packageJsonDataLib=$(cat libs/$folder/package.json)
+    libPackageVersion=$(echo "$packageJsonDataLib" | jq -r '.version')
+    packageJsonDataLib=$(echo "$packageJsonDataLib" | sed -E 's/(@onecx[^"]+?": *?")([^"]+)"/\1^'$1'"/')
+    echo $packageJsonDataLib > libs/$folder/package.json
+    if [[ $libPackageVersion != $1 ]]
+    then
+        npx -p replace-json-property rjp libs/$folder/package.json version $1
+        npx nx run $folder:release
+    fi  
+done
+
+
---
+++ release.config.js
@@ -0,0 +1,47 @@
+module.exports = {
+  branches: [
+    'main',
+    {
+      name: 'develop',
+      channel: 'rc',
+      prerelease: 'rc',
+    },
+  ],
+  preset: 'conventionalcommits',
+  presetConfig: {
+    types: [
+      { type: 'feat', section: 'Features' },
+      { type: 'fix', section: 'Bug Fixes' },
+      { type: 'chore', section: 'Chores' },
+      { type: 'docs', hidden: true },
+      { type: 'style', hidden: true },
+      { type: 'refactor', section: 'Refactoring' },
+      { type: 'perf', hidden: true },
+      { type: 'test', hidden: true },
+    ],
+  },
+  releaseRules: [{ type: 'refactor', release: 'patch' }],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: `./CHANGELOG.md`,
+      },
+    ],
+    [
+      '@semantic-release/exec',
+      {
+        prepareCmd: `./release-script.sh \${nextRelease.version} \${nextRelease.channel}`,
+      },
+    ],
+    [
+      '@semantic-release/git',
+      {
+        assets: [`libs/**/package.json`, `package.json`, `CHANGELOG.md`],
+        message: 'chore(release): -v${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+  ],
+}
